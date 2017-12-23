8836b51f4b832574edd93d8db55c7ca99fa9f656
HimanshuRami <ramihimanshu27@gmail.com>
2017-12-23 14:14:53 +0530

    Changes in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,17 +29,18 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+config.action_mailer.default_url_options = { :host => 'https://sampleappbyhemsh.herokuapp.com/' }
+config.action_mailer.raise_delivery_errors = true
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
   domain: "gmail.com",
   authentication: "plain",
-  enable_starttls_auto: true,
+  # enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
   password: ENV["GMAIL_PASSWORD"]
 }
-config.action_mailer.default_url_options = { :host => 'https://sampleappbyhemsh.herokuapp.com/' }
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
